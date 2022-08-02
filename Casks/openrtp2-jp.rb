bfe7c6a844b0d5ab8290e2a07ffbb55aaa5bedcc
Noriaki Ando <n-ando@n-andosMBP16M1.local>
2022-08-02 12:00:00 +0900

    .

--- Casks/openrtp2-jp.rb
+++ Casks/openrtp2-jp.rb
@@ -10,7 +10,8 @@ cask "openrtp2-jp" do
   version "2.0.0"
   sha256 "e84cdf01dd7186855e2be504f50a95b7d31cb48ec8182fd6d85220f0dd2923fa"
   
-  url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/openrtp2-jp_v2.0.0.dmg"
+  url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/openrtp2-jp_v2.0.0.dmg",
+      verified: "github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/"
   name "OpenRTP2"
   desc "Open RT Platform tool chain ver 2.0 (Japanese)"
   homepage "https://openrtm.org/"
@@ -18,6 +19,13 @@ cask "openrtp2-jp" do
   depends_on cask: "homebrew/cask-versions/adoptopenjdk8"
 
   app "OpenRTP2.app", target: "OpenRTP2-jp.app"
+
+  preflight do
+    system_command "chflags",
+                   args: ["nohidden", "#{staged_path}/OpenRTP2.app"]
+    system_command "xattr",
+                   args: ["-cr", "#{staged_path}/OpenRTP2.app"]
+  end
 
   caveats do
     depends_on_java "8"
--- Casks/openrtp2.rb
+++ Casks/openrtp2.rb
@@ -10,7 +10,8 @@ cask "openrtp2" do
   version "2.0.0"
   sha256 "c834695e59c8abd355799c041f6ce6c8a14451114a0b264b2ca76dd06de2e4fc"
 
-  url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/openrtp2-en_v2.0.0.dmg"
+  url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/openrtp2-en_v2.0.0.dmg",
+      verified: "github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.0/"
   name "OpenRTP2"
   desc "Open RT Platform tool chain ver 2.0"
   homepage "https://openrtm.org/"
