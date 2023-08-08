27c49cd16a55e873c558997bf22054f7eb8716c6
n-ando <Noriaki.Ando@gmail.com>
2023-08-08 12:00:00 +0900

    .

--- openrtm2-py39.rb
+++ openrtm2-py39.rb
@@ -4,7 +4,7 @@
 # Author: Noriaki Ando <Noriaki.Ando@gmail.com>
 # GitHub: https://github.com/OpenRTM/homebrew-openrtm
 #
-# This is the formula for OpenRTM-aist (C++) for python3.9.
+# This is the formula for OpenRTM-aist (C++) for python 3.9.
 # To use this formula/bottle, switch python into python 3.9.
 # $ brew unlink python3 (unlink python 3.X)
 # $ brew link python@3.9
@@ -17,18 +17,18 @@ class Openrtm2Py39 < Formula
   license "LGPL-2.1"
 
   bottle do
-    root_url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.1"
-    sha256 cellar: :any, arm64_ventura: "03e78724077df336c7ad02e6c01bd1427ed9a878ba58d41b34f10f41141dd026"
-    sha256 cellar: :any, monterey:      "f7e02e504ca4f1307f7d5fbf840e45d824cef93e7d3794ff927cdd1ac0952624"
+#    root_url "https://github.com/OpenRTM/homebrew-openrtm2/releases/download/2.0.1"
+#    sha256 cellar: :any, arm64_ventura: "03e78724077df336c7ad02e6c01bd1427ed9a878ba58d41b34f10f41141dd026"
+#    sha256 cellar: :any, monterey:      "f7e02e504ca4f1307f7d5fbf840e45d824cef93e7d3794ff927cdd1ac0952624"
   end
 
   depends_on "boost"
   depends_on "cmake" => :build
   depends_on "openrtm/omniorb/omniorb-ssl-py39"
-  https://github.com/OpenRTM/homebrew-openrtm2/blob/main/Patch/CMakeList.txt.diff
+
   patch do
     url "https://raw.githubusercontent.com/OpenRTM/homebrew-openrtm2/refs/heads/main/Patch/CMakeList.txt.diff"
-    sha256 "d0dc674e97682a16781c4a9c69dc5fe6809c5c55b8f47c0b627c97c7efc78506"
+    sha256 "d0dcd886155ae4f613c5ab10344e4a38486da8caee2e129fff0c54cdf8d8b7da"
   end
 
   def install
