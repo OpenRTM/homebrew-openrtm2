02571e63a44a164b80e4bf61372fb98831ed3202
n-ando <Noriaki.Ando@gmail.com>
2023-08-29 12:00:00 +0900

    .

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ build=(
     "openrtm2-py39  omniorb-ssl-py39"
     "openrtm2-py310 omniorb-ssl-py310"
     "openrtm2-py311 omniorb-ssl-py311"
-    "openrtm2-python-py38  omniorb-ssl-py38"
+    "openrtm2-py312 omniorb-ssl-py312"
+    "openrtm2-py313 omniorb-ssl-py313"
+
     "openrtm2-python-py39  omniorb-ssl-py39"
     "openrtm2-python-py310 omniorb-ssl-py310"
     "openrtm2-python-py311 omniorb-ssl-py311"
