e088c575f666549d4e55d0726eb81b83e01a6bab
n-ando <Noriaki.Ando@gmail.com>
2024-02-16 12:00:00 +0900

    .

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 
 build=(
     "openrtm2-py310 omniorb-ssl-py310"
-#    "openrtm2-py311 omniorb-ssl-py311"
-#    "openrtm2-py312 omniorb-ssl-py312"
-#    "openrtm2-py313 omniorb-ssl-py313"
-#    "openrtm2-py313 omniorb-ssl-py313"
+    "openrtm2-py311 omniorb-ssl-py311"
+    "openrtm2-py312 omniorb-ssl-py312"
+    "openrtm2-py313 omniorb-ssl-py313"
+    "openrtm2-py313 omniorb-ssl-py313"
 )
 
 bottle()
