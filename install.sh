11241d7769f0a0d0004f5af4a0c1d1c7aaf8b2b6
n-ando <Noriaki.Ando@gmail.com>
2024-02-28 12:00:00 +0900

    .

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 build=(
-    "openrtm2-py39  omniorb-ssl-py39"
     "openrtm2-py310 omniorb-ssl-py310"
     "openrtm2-py311 omniorb-ssl-py311"
     "openrtm2-py312 omniorb-ssl-py312"
     "openrtm2-py313 omniorb-ssl-py313"
+    "openrtm2-py314 omniorb-ssl-py314"
 
-    "openrtm2-python-py39  omniorb-ssl-py39"
     "openrtm2-python-py310 omniorb-ssl-py310"
     "openrtm2-python-py311 omniorb-ssl-py311"
     "openrtm2-python-py312 omniorb-ssl-py312"
     "openrtm2-python-py313 omniorb-ssl-py313"
+    "openrtm2-python-py314 omniorb-ssl-py314"
 )
 
 cleanup()
